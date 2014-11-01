eea0b55973600a4631c615e228f6649bdec6aeee
Craig Kochis <cjkochis@gmail.com>
2014-11-01 09:58:32 -0700

    add language specific syntax highlighting

---
+++ assets/js/syntax_highlighting.js
@@ -0,0 +1,5 @@
+$(function() {
+  ["css", "c", "javascript", "java", "php", "coffeescript", "sass", "bash", "python", "sql", "ruby", "objective-c"].forEach(function(language) {
+    $('code.'+language).attr('class', 'language-'+language);
+  });
+});
