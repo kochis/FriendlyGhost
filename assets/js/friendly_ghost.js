96e8d8edfa96335f757033c8df4f11fb6262e24a
Craig Kochis <cjkochis@gmail.com>
2014-11-04 23:39:35 -0800

    remove contact form

--- assets/js/friendly_ghost.js
+++ assets/js/friendly_ghost.js
@@ -3,19 +3,4 @@ $(function() {
   ["css", "c", "javascript", "java", "php", "coffeescript", "sass", "bash", "python", "sql", "ruby", "objective-c"].forEach(function(language) {
     $('code.'+language).attr('class', 'language-'+language);
   });
-
-  // Contact form submission
-  var contactForm = $('#contact-form');
-  var submitButton = $('#contact-submit-button');
-  submitButton.on('click', function() {
-    $.ajax({
-      type: "POST",
-      url: contactForm.attr('action'),
-      data: contactForm.serialize()
-    });
-    submitButton.fadeOut();
-    contactForm.fadeOut(function() {
-      $('#thank-you').fadeIn();
-    });
-  });
 });
